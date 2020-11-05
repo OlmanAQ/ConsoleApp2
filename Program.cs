a620b14401e455ba7327ed2d7649773efd8eabff
KristelSalas <Kristelsalas@estudianTEC.cr>
2020-11-04 19:38:53 -0600

    Librerias

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using ScrapySharp.Extensions;
 
 namespace ConsoleApp2
 {
-    class Program
+   class Program
     {
         static void Main(string[] args)
         {
