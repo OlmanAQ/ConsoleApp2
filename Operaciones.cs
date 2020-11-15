7aa6cb2d5d85807ffd6e7f142ab04647ab315b4b
KristelSalas <Kristelsalas@estudianTEC.cr>
2020-11-14 22:39:59 -0600

    archivos y comparaciones

--- Operaciones.cs
+++ Operaciones.cs
@@ -11,126 +11,103 @@ namespace ProyectoArqui
 {
     class Operaciones
     {
-
-        public void PrimeraPagina() // obtiene los titulos de todas las peliculas en la página sensacine
+        //recibe el titulo compara y devuelve la calificacion
+        public double PrimeraPagina(String nombre) // obtiene los titulos y calificacion de todas las peliculas en la página sensacine
         {
             List<String> lista = new List<String>();
             HtmlWeb web1 = new HtmlWeb();
 
             String url = "http://www.sensacine.com/peliculas/criticas-sensacine/";
             HtmlDocument document = web1.Load(url);
-            foreach (var nodo in document.DocumentNode.CssSelect(".meta-title-link"))
+
+            foreach (var nodo in document.DocumentNode.CssSelect(".card.entity-card.entity-card-list.cf"))
             {
-                lista.Add(nodo.InnerHtml);
+                var nodo2 = nodo.CssSelect(".meta-title-link").First();
+                //lista.Add(nodo2.InnerHtml);
+                var titulo = nodo2.InnerHtml;
+                if (titulo.Equals(nombre))
+                {
+                    var nodo3 = nodo.CssSelect(".stareval-note").First();                  
+                    lista.Add(nodo3.InnerHtml);
+                }
+                
             }
             url += "?page=";
-            for (int i = 1; i <= 410; i++) 
+            for (int i = 1; i <= 10; i++)//410
             {
                 int s = i;
                 String v = url + s;
                 document = web1.Load(v);
-                foreach (var nodo in document.DocumentNode.CssSelect(".meta-title-link"))
+                foreach (var nodo in document.DocumentNode.CssSelect(".card.entity-card.entity-card-list.cf"))
                 {
-                    lista.Add(nodo.InnerHtml);
-                }               
-            }
-
-            foreach (var item in lista)
-            {
-                Console.WriteLine("Nombre de película"+item);
-            }           
-        }
-
-        public void PrimeraCalificacion() // obtiene las calificaciones de todas las peliculas en la página sensacine
-        {
-            List<String> lista2 = new List<String>();
-            HtmlWeb web2 = new HtmlWeb();
-            String url = "http://www.sensacine.com/peliculas/criticas-sensacine/";
-            HtmlDocument document2 = web2.Load(url);
-
-            foreach (var nodo in document2.DocumentNode.CssSelect(".stareval-note"))
-            {
-                lista2.Add(nodo.InnerHtml);
-            }
-            url += "?page=";
-            for (int i = 1; i <= 410; i++)
-            {
-                int s = i;
-                String v = url + s;
-                document2 = web2.Load(v);
-                foreach (var nodo in document2.DocumentNode.CssSelect(".stareval-note"))
-                {
-                    lista2.Add(nodo.InnerHtml);
+                    var nodo2 = nodo.CssSelect(".meta-title-link").First();
+                    //lista.Add(nodo2.InnerHtml);
+                    var titulo = nodo2.InnerHtml;
+                    if (titulo.Equals(nombre))
+                    {
+                        var nodo3 = nodo.CssSelect(".stareval-note").First();
+                        lista.Add(nodo3.InnerHtml);
+                    }
                 }
             }
 
-            foreach (var item in lista2)
-            {
-                Console.WriteLine("nota de pelicula"+item);//van en el mismo orden que las peliculas
-            }
-
+            //foreach (var item in lista)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            double califinal = Convert.ToDouble(lista[0]);
+            return califinal;
         }
+       
 
-        public void SegundaPagina()  // obtiene los titulos de todas las peliculas en la página de metacritic
+
+        public int SegundaPagina(String nombre)  // obtiene los titulos y calificacion de todas las peliculas en la página de metacritic
         {
             List<String> lista3 = new List<String>();
             HtmlWeb web3 = new HtmlWeb();
             String url = "https://www.metacritic.com/browse/movies/score/metascore/all/filtered?sort=desc";
             HtmlDocument document3 = web3.Load(url);
 
-            foreach (var nodo in document3.DocumentNode.CssSelect("a.title"))
+            foreach (var nodo in document3.DocumentNode.CssSelect(".clamp-summary-wrap"))
             {
-                lista3.Add(nodo.InnerHtml);
+                var nodo2 = nodo.CssSelect("h3").First();
+                //lista3.Add(nodo2.InnerHtml);
+                var titulo = nodo2.InnerHtml;
+                if (titulo.Equals(nombre))
+                {
+                    var nodo3 = nodo.CssSelect(".metascore_w.large.movie").First();
+                    lista3.Add(nodo3.InnerHtml);
+                }               
             }
             url += "&page=";
-            for (int i = 1; i <= 137; i++)
+            for (int i = 1; i <= 10; i++)//137
             {
                 int s = i;
                 String v = url + s;
-                Console.WriteLine(v);
                 document3 = web3.Load(v);
-                foreach (var nodo in document3.DocumentNode.CssSelect("a.title"))
+                foreach (var nodo in document3.DocumentNode.CssSelect(".clamp-summary-wrap"))
                 {
-                    lista3.Add(nodo.InnerHtml);
+                    var nodo2 = nodo.CssSelect("h3").First();
+                    //lista3.Add(nodo2.InnerHtml);
+                    var titulo = nodo2.InnerHtml;
+                    if (titulo.Equals(nombre))
+                    {
+                        var nodo3 = nodo.CssSelect(".metascore_w.large.movie").First();
+                        lista3.Add(nodo3.InnerHtml);
+                    }                  
                 }
             }
 
-            foreach (var item in lista3)
-            {
-                Console.WriteLine("nombre de pelicula: " + item);//van en el mismo orden que las peliculas
-            }            
+            //foreach (var item in lista3)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            int califinal = Convert.ToInt32(lista3[0]);
+            return califinal;
         }
+       
 
-        public void SegundaCalificacion()  // obtiene las calificaciones de todas las peliculas en la página de metacritic
-        {
-            List<String> lista4 = new List<String>();
-            HtmlWeb web4 = new HtmlWeb();
-            String url = "https://www.metacritic.com/browse/movies/score/metascore/all/filtered?sort=desc";
-            HtmlDocument document4 = web4.Load(url);
 
-            foreach (var nodo in document4.DocumentNode.CssSelect(".metascore_w.large.movie"))
-            {
-                lista4.Add(nodo.InnerHtml);
-            }
-            url += "&page=";
-            for (int i = 1; i <= 137; i++)
-            {
-                int s = i;
-                String v = url + s;
-                document4 = web4.Load(v);
-                foreach (var nodo in document4.DocumentNode.CssSelect(".metascore_w.large.movie"))
-                {
-                    lista4.Add(nodo.InnerHtml);
-                }
-            }
-
-            foreach (var item in lista4)
-            {
-                Console.WriteLine("nota de pelicula: " + item);//van en el mismo orden que las peliculas
-            }
-        }
-
-        //Esta aun no obtiene solo la fecha de lanzamiento.
         public void TerceraPagina()  // obtiene la fecha de lanzamiento de todas las peliculas en la página de metacritic
         {
             List<String> lista5 = new List<String>();
@@ -138,28 +115,34 @@ namespace ProyectoArqui
             String url = "https://www.metacritic.com/browse/movies/score/metascore/all/filtered?sort=desc";
             HtmlDocument document5 = web5.Load(url);
 
-            foreach (var nodo in document5.DocumentNode.CssSelect(".clamp-details"))
+            foreach (var nodo in document5.DocumentNode.CssSelect(".clamp-summary-wrap"))
             {
-                lista5.Add(nodo.InnerHtml);
+                var nodo2 = nodo.CssSelect("h3").First();
+                lista5.Add(nodo2.InnerHtml);
+                var nodo3 = nodo.CssSelect(".clamp-details");
+                var nodo4 = nodo3.CssSelect("span").First();
+                lista5.Add(nodo4.InnerHtml);
             }
             url += "&page=";
-            for (int i = 1; i <= 20; i++)
+            for (int i = 1; i <= 10; i++)
             {
                 int s = i;
                 String v = url + s;
                 document5 = web5.Load(v);
-                foreach (var nodo in document5.DocumentNode.CssSelect(".clamp-details"))
+                foreach (var nodo in document5.DocumentNode.CssSelect(".clamp-summary-wrap"))
                 {
-                    lista5.Add(nodo.InnerHtml);
+                    var nodo2 = nodo.CssSelect("h3").First();
+                    lista5.Add(nodo2.InnerHtml);
+                    var nodo3 = nodo.CssSelect(".clamp-details");
+                    var nodo4 = nodo3.CssSelect("span").First();
+                    lista5.Add(nodo4.InnerHtml);
                 }
             }
 
             foreach (var item in lista5)
             {
-                Console.WriteLine("nota de pelicula: " + item);//van en el mismo orden que las peliculas
+                Console.WriteLine("nota de pelicula: " + item);
             }
-
-
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 using ScrapySharp.Extensions;
+using System.IO;
 
 namespace ProyectoArqui
 {
@@ -12,16 +13,60 @@ namespace ProyectoArqui
     {
         static void Main(string[] args)
         {
-            //Operaciones prueba = new Operaciones();
+            Operaciones prueba = new Operaciones();
             //prueba.PrimeraPagina();
-            //Operaciones prueba2 = new Operaciones();
-            //prueba2.PrimeraCalificacion();
-            //Operaciones prueba3 = new Operaciones();
-            //prueba3.SegundaPagina();
-            //Operaciones prueba4 = new Operaciones();
-            //prueba4.SegundaCalificacion();
-            Operaciones prueba5 = new Operaciones();
-            prueba5.TerceraPagina();
+            //prueba.SegundaPagina();
+            //prueba.TerceraPagina();
+
+            //el error puede ser que la lista que mando de vyuelta con la calificacion tenga que cambair o no quien sabe mañana me dare
+            //cuenta xd
+
+
+            String linea;
+            try
+            {
+                //Pasa la ruta del archivo y el nombre del archivo al constructor StreamReader
+                StreamReader sr = new StreamReader("C:\\Users\\Kristel Salas\\source\\repos\\ProyectoArqui\\Peliculas.txt");//direccion local en donde se guarda el archivo
+                linea = sr.ReadLine(); //Lee la primera línea de texto
+                while (linea != null) //Continúa leyendo hasta llegar al final del archivo
+                {
+                    //Console.WriteLine(linea); //escribe la linea en la ventana de la consola
+                    double cali = prueba.PrimeraPagina(linea); //se manda el tiulo y se devuelve la calificacion
+                    int cali2 = prueba.SegundaPagina(linea); //se manda titulo y devuelve calificacion
+                    Console.WriteLine("nombre de la película:"+linea + " Calificacion de la pelicula: " +cali);
+                    Console.WriteLine("nombre de la película:" + linea + " Calificacion de la pelicula: " + cali2);
+                    linea = sr.ReadLine(); //Lee la siguiente línea
+                }               
+                sr.Close();//cierra el archivo
+            }
+            catch (ArgumentOutOfRangeException outOfRange)
+            {
+                Console.WriteLine("Exception: " + outOfRange.Message);
+                //Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Fin del archivo");
+            }
+
+            //Parallel.Invoke(
+            //    () =>
+            //    {
+            //        //Nombre de la funcion
+
+            //    },  // cierre de la primera accion
+            //    () =>
+            //    {
+            //        //nombre de la funcion
+
+            //    }, //cierre de la segunda accion
+            //    () =>
+            //    {
+            //        //nombre de la funcion
+
+            //    } //cierre de la tercera accion
+            //); //cierre del parallel invoke
+
         }
     }
 }
